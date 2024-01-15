c71b222b8c62a3e900ca3704505666bb25478b3d
xx1003 <wntnwls2002@gmail.com>
2024-01-15 17:20:09 +0900

    v0.8 python day02

--- day02.py
+++ day02.py
@@ -1,13 +1,9 @@
-base_number = int(input('Input base number : '))
-exponent_number = int(input('Input exponent number : '))
-print(type(base_number), type(exponent_number))
+first_number = int(input('Input first number : '))
+second_number = int(input('Input second number : '))
 
-# f-string
-print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number**exponent_number}')
+# divmod(first, second) => (몫, 값) tuple
+print(f'몫은 {divmod(first_number,second_number)[0]}, 나머지는 {divmod(first_number, second_number)[1]}')
 
-# format function
-print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
-print('밑은 {}, 지수는 {}, 결과 값은 {}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
-
-# c like
-print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number, exponent_number)))
+# quotient = first_number // second_number
+# remainder = first_number % second_number
+# print(f'몫은 {quotient}, 나머지는 {remainder}')
