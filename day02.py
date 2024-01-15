23e17b60b852784d7e1fdbb9e6b037e6d9460038
xx1003 <wntnwls2002@gmail.com>
2024-01-15 16:48:41 +0900

    v0.3 python day02

---
+++ day02.py
@@ -0,0 +1,18 @@
+# print(1e-1)  # 0.1
+# print(314e-2)  # 3.14
+# print(21000)
+# print(21_000)  # 21000
+
+# univ = "Inha university"  # immutable
+# univ[5] = 'U'  # TypeError: 'str' object does not support item assignment
+# print(univ)
+
+subjects = ['python', 'c++', 'linux', 'data structure', 'database']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'data structure & algorithm'  # mutable
+print(subjects)
+print(subjects[3])
+
+
+
