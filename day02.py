7a22073c609144f0541c8c7bbbd86714e81137bf
xx1003 <wntnwls2002@gmail.com>
2024-01-15 17:27:40 +0900

    v0.9 python day02

--- day02.py
+++ day02.py
@@ -1,9 +1,8 @@
-first_number = int(input('Input first number : '))
-second_number = int(input('Input second number : '))
-
-# divmod(first, second) => (몫, 값) tuple
-print(f'몫은 {divmod(first_number,second_number)[0]}, 나머지는 {divmod(first_number, second_number)[1]}')
-
-# quotient = first_number // second_number
-# remainder = first_number % second_number
-# print(f'몫은 {quotient}, 나머지는 {remainder}')
+dec = 65
+octal = 0o101
+hexadecimal = 0x41
+binary = 0b01000001
+print(dec, octal, hexadecimal, binary)
+print(chr(dec), chr(octal), chr(hexadecimal), chr(binary))  # ASCII
+print(bin(dec), bin(octal), bin(hexadecimal), bin(binary))
+print(ord('B'), ord('Z'), ord('a'), ord('2'))  # ASCII -> int?
