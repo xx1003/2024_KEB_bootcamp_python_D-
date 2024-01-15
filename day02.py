8a5c3a9741083a18e31cc6e1fda73f4d5f757866
xx1003 <wntnwls2002@gmail.com>
2024-01-15 17:14:14 +0900

    v0.7 python day02

--- day02.py
+++ day02.py
@@ -1,10 +1,13 @@
-# 거듭제곱
 base_number = int(input('Input base number : '))
 exponent_number = int(input('Input exponent number : '))
+print(type(base_number), type(exponent_number))
+
+# f-string
 print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number**exponent_number}')
-# money = 5,000,000
-# print(money)  # (5, 0, 0)
-# print(type(money))  # tuple
-# cash = 5_000_000
-# print(cash)  # 5000000
-# print(type(cash))  # int
+
+# format function
+print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
+print('밑은 {}, 지수는 {}, 결과 값은 {}'.format(base_number, exponent_number, pow(base_number, exponent_number)))
+
+# c like
+print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number, exponent_number)))
