3ccdc4d5593732f72804bcef170ad6fae102d717
xx1003 <wntnwls2002@gmail.com>
2024-01-16 16:30:36 +0900

    day03 assignment01

--- day03.py
+++ day03.py
@@ -1,19 +1,51 @@
-# prime number
-numbers = input("Input first second number : ").split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
+while True:
+    menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit  3) Is prime number?  "
+                 "4) Between prime number  5) Quit Program : ")
 
-if n1 > n2:
-    n1, n2 = n2, n1  # swap 을 이딴 식으로 하네..
+    if menu == '1':
+        fahrenheit = float(input("Input Fahrenheit : "))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0/9.0):.4f}C')
 
-for number in range(n1, n2 + 1):
-    isPrime = True
+    elif menu == '2':
+        celsius = float(input("Input Celsius : "))
+        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius * (9.0/5.0)) + 32.0):.4f}F')
 
-    if number < 2:
-        pass
-    else:
-        for i in range(2, number):
-            if number % i == 0:
-                isPrime = False
-                break
-        if isPrime: print(number, end = ' ')
+    elif menu == '3':
+        number = int(input("Input number : "))
+        isPrime = True
+        if number < 2:
+            isPrime = False
+        else:
+            for i in range(2, number):
+                if number % i == 0:
+                    isPrime = False
+                    break
+                i += 1
+
+        if isPrime:
+            print(f'{number} is prime number')
+        else:
+            print(f'{number} is NOT prime number!')
+
+    elif menu == '4':
+        numbers = input("Input first & second number : ").split()
+        n1 = int(numbers[0])
+        n2 = int(numbers[1])
+
+        if n1 > n2:
+            n1, n2 = n2, n1
+
+        for number in range(n1, n2 + 1):
+            if number < 2:
+                continue
+            else:
+                for i in range(2, number):
+                    if number % i == 0:
+                        break
+                else:
+                    print(number, end = ' ')
+        print()
+
+    elif menu == '5':
+        print("Terminate Program.")
+        break
