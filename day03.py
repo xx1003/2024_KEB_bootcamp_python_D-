7684df1e04c5ecc39a5177587ee7e1b2e02c9dbf
xx1003 <wntnwls2002@gmail.com>
2024-01-16 14:59:17 +0900

    v2.7 day03

--- day03.py
+++ day03.py
@@ -1,18 +1,17 @@
-# prime number
-number = int(input("Input number : "))
-isPrime = True
+univ = "inha"
+i = 0
+while i < len(univ):
+    print(univ[i], end = ' ')
+    i += 1
 
-if number < 2:
-    print(f'{number} is NOT prime number!')
-else:
-    i = 2
-    while i < number:
-        if number % i == 0:
-            isPrime = False
-            break
-        i += 1
+print()
 
-    if isPrime:
-        print(f'{number} is prime number')
-    else:
-        print(f'{number} is NOT prime number!')
+for letter in univ:
+    print(letter, end = ' ')
+
+print()
+
+# for k in range(0, len(univ), 1):
+# for k in range(0, len(univ)):
+for k in range(len(univ)):
+    print(univ[k], end = ' ')
