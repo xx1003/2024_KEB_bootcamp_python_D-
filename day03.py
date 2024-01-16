57243b269fcc107949375dbbcba39d7c8cf01627
xx1003 <wntnwls2002@gmail.com>
2024-01-16 15:43:31 +0900

    v3.0 prime number day03

--- day03.py
+++ day03.py
@@ -3,6 +3,9 @@ numbers = input("Input first second number : ").split()
 n1 = int(numbers[0])
 n2 = int(numbers[1])
 
+if n1 > n2:
+    n1, n2 = n2, n1  # swap 을 이딴 식으로 하네..
+
 for number in range(n1, n2 + 1):
     isPrime = True
 
@@ -13,5 +16,4 @@ for number in range(n1, n2 + 1):
             if number % i == 0:
                 isPrime = False
                 break
-        if isPrime : print(number, end = ' ')
-        # else : print(number)
+        if isPrime: print(number, end = ' ')
