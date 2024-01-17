ba52fe28aa01382dc5f08f92fdce91cb982d9cc6
xx1003 <wntnwls2002@gmail.com>
2024-01-17 13:33:54 +0900

    v3.8 day04

--- day04.py
+++ day04.py
@@ -1,13 +1,11 @@
-subjects = ["데이터베이스", "c++", "5", "java", "python", "java", "9", "리눅스"]
-# print(subjects[::-1])
-# subjects = subjects[::-1]
-# print(subjects)
-# subjects.reverse()
-# print(subjects)
-# subjects.remove("java")
-# del subjects[2]
-# subjects.sort()  # 원본이 바뀜
-# subjects.sort(reverse=True)  # desc
-copy_subjects = sorted(subjects)
-print(subjects)
-print(copy_subjects)
+import copy
+# subjects = ["a", "b", "c"]
+subjects = ["a", ["b", "c"], "d"]
+a = subjects
+b = subjects.copy()
+c = list(subjects)
+d = subjects[:]
+e = copy.deepcopy(a)
+print(subjects, a, b, c, d, e)
+subjects[1][1] = "x"
+print(subjects, a, b, c, d, e)
