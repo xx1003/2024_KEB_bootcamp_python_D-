954600148811832e60d3f09f96e123a9a1517dc9
xx1003 <wntnwls2002@gmail.com>
2024-01-17 09:46:17 +0900

    v3.3 day04

--- day04.py
+++ day04.py
@@ -1,51 +1,22 @@
-# Assignment
+numbers = input("Input first second number : ").split()  # 두 수 입력 못 받으면 에러?,  간소화 가능
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
-while True:
-    menu = input('1) Fahrenheit -> Celsius  2) Celsius -> Fahrenheit  3) Prime1  4) Prime2  5) Quit program : ')
+if n1 > n2:
+    n1, n2 = n2, n1
+for number in range(n1, n2 + 1):
+    isPrime = True
 
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}C')
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius * 9.0/5.0) + 32.0):.4f}F')
-    elif menu == '3':
-        number = int(input("Input number : "))
-        isPrime = True
-
-        if number < 2:
-            print(f'{number} is NOT prime number!')
-        else:
-            for i in range(2, number):
-                if number % i == 0:
-                    isPrime = False
-                    break
-
-            if isPrime:
-                print(f'{number} is prime number')
-            else:
-                print(f'{number} is NOT prime number!')
-    elif menu == '4':
-        numbers = input("Input first second number : ").split()
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
-
-        if n1 > n2:
-            n1, n2 = n2, n1
-        for number in range(n1, n2 + 1):
-            isPrime = True
-
-            if number < 2:
-                pass
-            else:
-                for i in range(2, number):
-                    if number % i == 0:
-                        isPrime = False
-                        break
-                if isPrime: print(number, end=' ')
-        print()
-    elif menu == '5':
-        print('Terminate Program.')
-        break
+    if number < 2:
+            continue
     else:
-        print('Invalid Menu!')
+        i = 2
+        while i*i < number:  # 반복 횟수 줄임
+            if number % i == 0:
+                isPrime = False
+                break
+            i += 1
+            # print(i, end = ' ')  # loop count
+        if isPrime:
+            print(number, end=' ')
+print()
