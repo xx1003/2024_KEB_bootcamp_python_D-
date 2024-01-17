7060aa30e6044a90397b699e09168f908a99cf50
xx1003 <wntnwls2002@gmail.com>
2024-01-17 14:43:16 +0900

    v4.0 day04

--- day04.py
+++ day04.py
@@ -1,11 +1,21 @@
-# squares = list()
-# for i in range(1,6):
-#     squares.append(i*i)
-# print(squares)
+sugang = dict(python="kim", cpp="sung", db="kang")
+# print(sugang)
+# sugang['datastructure'] = 'kim'
+# print(sugang)
+# sugang['datastructure'] = 'park'
+# print(sugang)
+# print(sugang['db'])
+# print(sugang.get('db'))
+# print(sugang.get('opensource', 'not exist'))
+for subject, professor in sugang.items():
+    print(f'{subject} 과목 담당교수는 {professor}입니다.')
 
-# list comprehension
-# squares = [i*i for i in range(1, 6, 1)]
-# print(squares)
+# for k in sugang.keys():
+for k in sugang:
+    print(k)
 
-even = [i*i for i in range(1, 6, 1) if i % 2 == 0]
-print(even)
+for v in sugang.values():
+    print(v)
+
+for s in sugang.items():
+    print(s)
