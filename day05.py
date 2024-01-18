cdcd264bf4d793e63f9cf678c02f84c8f92418bc
xx1003 <wntnwls2002@gmail.com>
2024-01-18 18:32:17 +0900

    day05 assignment

--- day05.py
+++ day05.py
@@ -1,10 +1,48 @@
-# def squares(n):
-#     return n * n
+# 9.1
+# def good():
+#     return ['Harry', 'Ron', 'Hermione']
+# print(good())
 
-even_numbers = [i for i in range(51) if i % 2 == 0]
-print(even_numbers)
-# print(tuple(map(squares, even_numbers)))
+def good(harry_potter):
+    return harry_potter
+print(good(['Harry', 'Ron', 'Hermione']))
 
-# print(tuple(map(lambda x: x**2, even_numbers)))
-z = lambda x: pow(x, 2)
-print(tuple(map(z, even_numbers)))
+# 9.2
+def get_odds() -> list:
+    """
+    range(10)에서 홀수만 골라 리스트에 집어넣고 리스트 반환
+    :return: list
+    """
+    return [i for i in range(10) if i % 2 != 0]
+
+get_list = get_odds()
+print(get_list[2])
+
+# 9.3
+def test(func):
+    def inner(*args):
+        print("start")
+        func(*args)
+        print("end")
+    return inner
+
+@test
+def cout(*args):
+    print(args)
+
+cout(1,2,3)
+
+# 9.4
+class OopsException(Exception):
+    print('Caught an oops')
+talking = ['Yes', 'No', 'Hello', 'Oops']
+for talk in talking:
+    if talk == 'Oops':
+        raise OopsException(talk)
+
+try:
+    raise OopsException('Caught an oops')
+except OopsException as exc:
+    print(exc)
+
+
