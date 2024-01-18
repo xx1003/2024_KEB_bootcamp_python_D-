9a0e463607845b2c0f2a64f5749b5c32887948c0
xx1003 <wntnwls2002@gmail.com>
2024-01-18 18:08:42 +0900

    dict & set practice

--- prac.py
+++ prac.py
@@ -1,17 +1,30 @@
-start1 = ["fee", "fie", "foe"]
-rhymes = [
-    ("flop","get a mop"),
-    ("fope", "turn the rope"),
-    ("fa", "get your ma"),
-    ("fudge", "call the judge"),
-    ("fat", "pet the cat"),
-    ("fog", "walk the dog"),
-    ("fun", "say we're done"),
-]
-start2 = "Someone better"
+# 8.11
+odd = {i for i in range(10) if i % 2 != 0}
+print(odd)
 
-for start in start1:
-    start = start.title()
+# 8.12
+letters = (letter for letter in "Got")
+for letter in letters:
+    print(letter, end=' ')
+print()
+numbers = (number for number in range(10))
+for number in numbers:
+    print(number, end=' ')
+print()
 
-print(start1)
-'! '.join(start1)
+# 8.13
+keys = ('optimist', 'pessimist', 'troll')
+values = ('The glass is half full', 'The glass is halt empty', 'How did you get a glass?')
+d = dict()
+for key, value in zip(keys,values):
+    d[key]=value
+print(d)
+
+d2 = {key:value for key, value in zip(keys,values)}
+print(d2)
+
+# 8.14
+titles = ['Creature of Habit', 'Crewel Fate']
+plots = ['A nun turns into a monster', 'A haunted yarn shop']
+movies = {title:plot for title, plot in zip(titles, plots)}
+print(movies)
