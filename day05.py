a231b2ac22d266308137110f4b05870a64de3922
xx1003 <wntnwls2002@gmail.com>
2024-01-18 09:30:04 +0900

    v4.3 assignment day05

---
+++ day05.py
@@ -0,0 +1,15 @@
+# Assignment ex 8.10
+# squares = {n:pow(n,2) for n in range(10)}
+# squares = {n:n**2 for n in range(10)}
+# print(squares)
+
+
+# univ = "Inha university"
+# counts_alphabet = {letter: univ.count(letter) for letter in univ}
+# print(counts_alphabet)
+
+# univ = "Inha university"
+# counts_alphabet = dict()
+# for letter in univ:
+#     counts_alphabet[letter] = univ.count(letter)
+# print(counts_alphabet)
