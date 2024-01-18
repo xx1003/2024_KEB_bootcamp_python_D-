1bb53c8ef12b7f3cddfdb30b5f8677d33f4753e8
xx1003 <wntnwls2002@gmail.com>
2024-01-18 14:18:40 +0900

    v4.8 day05

--- day05.py
+++ day05.py
@@ -1,19 +1,8 @@
-# Closure
-def out_func(nout):
-    def in_func():
-        return nout * nout
-    return in_func
+numbers = ["7", "-11", "3"]
+print(sum(map(int, numbers)))
 
-
-x = out_func(9)  # 내부 함수 리턴? 그래서 x 는 함수 (inner function 주소를 받음)
-print(type(x))
-print(x)
-print(x())
-
-# Inner function
-# def out_func(nout):
-#     def in_func(nin):
-#         return nin * nin
-#     return in_func(nout)
-#
-# print(out_func(5))
+# numbers = ["7", "-11", "3"]
+# hap = 0
+# for number in numbers:
+#     hap += int(number)
+# print(hap)
