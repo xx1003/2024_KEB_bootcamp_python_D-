be300bf2b91c3844bc04808f19466ed1e3fa9490
xx1003 <wntnwls2002@gmail.com>
2024-01-19 15:10:02 +0900

    v6.2 day06

--- day06.py
+++ day06.py
@@ -1,38 +1,28 @@
+class FlyingMixin:
+    def fly(self):
+        return f"{self.name}이(가) 하늘을 훨훨 날아갑니다~"
+
+
+class SwimmingMixin:
+    def swim(self):
+        return f"{self.name}이(가) 수영을 합니다."
+
+
 class Pokemon:
     def __init__(self, name):
         self.name = name
 
-    def attack(self, target):
-        print(f'{self.name}이(가) {target.name}을(를) 공격!')
 
-
-class Pikachu(Pokemon):  # is-a 상속 관계
-    def __init__(self, name, type):
-        super().__init__(name)
-        self.type = type
-
-    def attack(self, target):
-        print(f'{self.name}이(가) {target.name}을(를) {self.type} 공격!')  # override
-
-    def electric_info(self):
-        print("전기 계열의 공격을 합니다.")
-
-
-class Squirtle(Pokemon):  # is-a 상속 관계
-   pass
-
-
-class Agumon:
+class Charizard(Pokemon, FlyingMixin):
     pass
 
 
-p1 = Pikachu('피카츄', '전기')
-p2 = Squirtle('꼬부기')
-p3 = Pokemon('아무개')
-p1.electric_info()
-# p3.electric_info()
-p1.attack(p2)
-p2.attack(p1)
-print(p1.name, p1.type)
-print(issubclass(Pikachu, Pokemon))
-print(issubclass(Agumon, Pokemon))
+class Garados(Pokemon, SwimmingMixin):
+    pass
+
+
+g1  = Garados("갸라도스")
+c1 = Charizard("리자몽")
+
+print(g1.swim())
+print(c1.fly())
