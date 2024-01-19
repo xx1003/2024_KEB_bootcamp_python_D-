5666bda0f82c963bd4eaf3763d6f9fa3adbf4c50
xx1003 <wntnwls2002@gmail.com>
2024-01-19 13:45:11 +0900

    v5.9 day06

--- day06.py
+++ day06.py
@@ -1,28 +1,29 @@
-import random
+# class Pokemon:
+#     pass
+#
+# pikachu = Pokemon()
+# squirtle = Pokemon()
+# pikachu.name = "피카츄"
+# pikachu.nemesis = squirtle
+# print(pikachu.name)
+# # print(pikachu.nemesis.name)
+# pikachu.nemesis.name = "꼬부기"
+# # squirtle.name = "꼬부기"
+# print(pikachu.nemesis.name)
+# print(squirtle.name)
 
-class OopsException(Exception):
-    pass
+# class Pokemon():
+class Pokemon:
+    def __init__(self, name):  # self == this
+        self.name = name
+        print(f"{name} 포켓몬스터 생성")
 
-# numbers = list()
-# for i in range(5):
-#     numbers.append(random.randint(1, 100))
-numbers = [random.randint(1, 100) for i in range(10)]
-print(numbers)
+    def attack(self, target):
+        print(f'{self.name}이(가) {target.name}을(를) 공격!')
 
-try:
-    pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
-    print(numbers[pick])
-    print(5//2)
-    raise OopsException("Oops")  # exception 예외 강제 발생
-except IndexError as err:
-    print(f"Wrong index number\n{err}")
-except ValueError as err:
-    print(f"Input Only Number~\n{err}")
-except ZeroDivisionError as err:
-    print(f"The denominator cannot be 0.\n{err}")
-except OopsException as err:
-    print(f"Oops Oops {err}")
-except Exception as err:
-    print(f"Error occurs : {err}")
-else:
-    print(f"Program terminate")
+charizard = Pokemon("리자몽")
+pikachu = Pokemon("피카츄")
+squirtle = Pokemon("꼬부기")
+charizard.attack(squirtle)
+# print(pikachu.name)
+# print(squirtle.name)
