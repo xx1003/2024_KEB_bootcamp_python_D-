432bb900c6cebb663026fa77c686d03e253c2bce
xx1003 <wntnwls2002@gmail.com>
2024-01-19 11:12:20 +0900

    v5.8 day06

--- day06.py
+++ day06.py
@@ -1,20 +1,28 @@
 import random
 
+class OopsException(Exception):
+    pass
+
 # numbers = list()
 # for i in range(5):
-#     numbers.append(random.randint(1,100))
+#     numbers.append(random.randint(1, 100))
 numbers = [random.randint(1, 100) for i in range(10)]
 print(numbers)
 
 try:
-    pick = int(input(f"Input index(0 ~ {len(numbers)-1}) : "))
+    pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
     print(numbers[pick])
-    print(5/0)
+    print(5//2)
+    raise OopsException("Oops")  # exception 예외 강제 발생
 except IndexError as err:
-    print(f"Out of range : Wrong index number\n{err}")
+    print(f"Wrong index number\n{err}")
 except ValueError as err:
     print(f"Input Only Number~\n{err}")
 except ZeroDivisionError as err:
     print(f"The denominator cannot be 0.\n{err}")
+except OopsException as err:
+    print(f"Oops Oops {err}")
 except Exception as err:
     print(f"Error occurs : {err}")
+else:
+    print(f"Program terminate")
