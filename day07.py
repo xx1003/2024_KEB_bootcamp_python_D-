ff5d43ac6c91d1bd4a3c9729f0bb5e841cfac8de
xx1003 <wntnwls2002@gmail.com>
2024-01-22 11:36:34 +0900

    v7.4 day07

--- day07.py
+++ day07.py
@@ -7,25 +7,23 @@ class JetPack(FlyingBehavior):
     def fly(self):
         return f"로켓추진기로 하늘을 날아갑니다!"
 
+
 class NoFly(FlyingBehavior):
     def fly(self):
         return f"하늘을 날 수 없습니다."
+
 
 class FlyWithWings(FlyingBehavior):
     def fly(self):
         return f"날개로 하늘을 훨훨 날아갑니다."
 
-class SwimmingBehavior:
-    def swim(self):
-        return f"{self.__name}이(가) 수영을 합니다."
 
 class Pikachu:
-    def __init__(self, name, hp, fly):
+    def __init__(self, name, hp):
         self.name = name
         self.hp = hp
-        self.fly_behavior = fly  # class 객체 값을 받음, aggregation
+        self.fly_behavior = NoFly()  # composition
 
 
-nofly = NoFly()
-p1 = Pikachu("피카츄", 35, nofly)
+p1 = Pikachu("피카츄", 35)
 print(p1.fly_behavior.fly())
